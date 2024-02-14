8bfb731f761c996ac078ce5c61a27347874eafb5
Nazar Montsibovych <nazar_montsibovych@epam.com>
2024-02-14 02:08:40 +0200

    Add PrimeVue and sketch for pages

---
+++ components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <Menubar :model="items" />
+  </div>
+</template>
+
+<script setup>
+import { PrimeIcons } from "primevue/api";
+import { ref } from "vue";
+
+const items = ref([
+  {
+    label: "Home",
+    icon: PrimeIcons.HOME,
+  },
+  {
+    label: "Film Library",
+    icon: PrimeIcons.LIST,
+  },
+  {
+    label: "About",
+    icon: PrimeIcons.INFO,
+  },
+  {
+    label: "Contact",
+    icon: "pi pi-envelope",
+  },
+]);
+</script>
